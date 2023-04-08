e6236194802cf6ad2f2c7edc7c1eb80a48c8e3bc
Anton Petrenko <admin@xoma.pro>
2023-04-08 22:24:17 +0300

    Update Program.cs
    ver 1.0.0.1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("sdfsdfsdfsdsdsdsdsfdsf");
+Console.WriteLine("sdfsdfsdfsdsdsdsdsfdsf");
+Console.WriteLine("sdfsdfsdfsdsdsdsdsfdsf");
+Console.WriteLine("sdfsdfsdfsdsdsdsdsfdsf");
+Console.WriteLine("ver 1.0.0.1");
